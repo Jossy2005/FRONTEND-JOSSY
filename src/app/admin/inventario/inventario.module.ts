5e1f54d1eb34e685525b0b06a398d29e7b3a9a61
PamelaPogo <pme.pogo@yavirac.edu.ec>
2024-12-04 07:32:38 -0500

    first commit

---
+++ src/app/admin/inventario/inventario.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductoComponent } from './components/producto/producto.component';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [
+    ProductoComponent,
+    CategoriaComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ]
+})
+export class InventarioModule { }
